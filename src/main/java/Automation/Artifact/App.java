d631560bbd7a0f49993ff90fe902b140abdf8057
Tarun Reddy <tarunre12@gmail.com>
2026-02-14 14:53:03 +0530

    second commit

--- src/main/java/Automation/Artifact/App.java
+++ src/main/java/Automation/Artifact/App.java
@@ -5,6 +5,6 @@ package Automation.Artifact;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Jenkins!!");
     }
 }
